ecc69c8b6b9df55f1346496c2c3aef2d0ab43b04
Huy-Tuan <sieuka1990@gmail.com>
2024-12-15 19:53:34 +0700

    Add files via upload

--- ss16b5.c
+++ ss16b5.c
@@ -1,51 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void updateValue(int **a, int *size, int newValue, int idex){
-		if(idex > *size || idex < 0){
-			printf("Vi tri khong hop le");
-			return;
-		}
-		*a = (int *)realloc(*a, (*size + 1) * sizeof(int));
-		if (*a == NULL){
-			printf("Khong the cap phat bo nho\n");
-			return;
-		}
-		for (int i = *size; i > idex; i--){
-			(*a)[i] = (*a)[i - 1];
-		}
-		(*a)[idex] = newValue;
-		(*size)++;
+void updateElement(int *arr, int size, int newValue, int position) {
+    if (position < 0 || position >= size) {
+        printf("Vi tri khong hop le!\n");
+        return;
+    }
+    *(arr + position) = newValue;
 }
-void printfArr(int *a, int size){
-	for(int i =0; i < size; i++){
-		printf("%d\t", a[i]);
-	}
-	printf("\n");
+
+int main() {
+    int arr[] = {5, 10, 15, 20, 25};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Mang truoc khi cap nhat: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", *(arr + i));
+    }
+    printf("\n");
+
+    int newValue = 99;
+    int position = 2;
+    updateElement(arr, size, newValue, position);
+
+    printf("Mang sau khi cap nhat: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", *(arr + i));
+    }
+    printf("\n");
+
+    return 0;
 }
-int main (){
-	int size = 5;
-	int *arr = (int *)malloc(size * sizeof(int));
-	if(arr == NULL){
-		printf("Khong the cap phat bo nho");
-		return;
-	}
-	for(int i = 0; i < 5; i++){
-		arr[i] = i + 1;
-	}
-	printf("Mang ban dau la: \n");
-	printfArr(arr, size);
-	
-	int newValue, idex;
-	
-	printf("Hay nhap phan tu can them ");
-	scanf("%d", &newValue);
-	printf("\nHay nhap vi tri muon them ");
-	scanf("%d", &idex);
-	
-	updateValue(&arr, &size, newValue, idex);
-	printf("Mang sau khi them phan tu la: \n");
-	printfArr(arr, size);
-	free(arr);
-	return 0;
-}
+
